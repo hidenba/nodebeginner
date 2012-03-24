3d983ab138496ca7aed26c550af418d5d3040ccc
HIDEKUNI Kajita <hide.nba@gmail.com>
2012-03-25 00:53:07 +0900

    change requesthandler

--- router.js
+++ router.js
@@ -1,5 +1,10 @@
-function route(patuname) {
-  console.log("Abount to route a request for " + patuname);
+function route(handle, pathname) {
+  console.log("Abount to route a request for " + pathname);
+  if(typeof handle[pathname] === 'function' ) {
+    handle[pathname]();
+  } else {
+    console.log("No request handler found for " + pathname);
+  }
 }
 
 exports.route = route;
