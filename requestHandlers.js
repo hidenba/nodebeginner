a00c4be49062d399b76fa039728577d13824b238
HIDEKUNI Kajita <hide.nba@gmail.com>
2012-03-25 01:24:39 +0900

    find command

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,11 +3,13 @@ var exec = require("child_process").exec;
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  exec("ls -lah", function(error, stdout, stderr){
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write(stdout);
-    response.end();
-  });
+  exec("find /",
+       {timeput: 10000, maxBuffer: 20000*1024},
+       function(error, stdout, stderr){
+         response.writeHead(200, {"Content-Type":"text/plain"});
+         response.write(stdout);
+         response.end();
+       });
 }
 
 function upload(response) {
