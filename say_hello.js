49097c04da1d30ea224b78fda6504ec8c43e2772
HIDEKUNI Kajita <hide.nba@gmail.com>
2012-03-25 00:11:07 +0900

    anonymous function

--- say_hello.js
+++ say_hello.js
@@ -1,9 +1,5 @@
-function say(word) {
-  console.log(word);
-}
-
 function execute(someFunction, value) {
   someFunction(value);
 }
 
-execute(say, "hello");
+execute(function(word){ console.log(word); }, "Hello");
