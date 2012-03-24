e3f36f6732438ce2ecb841798370c4dd4ff87c66
HIDEKUNI Kajita <hide.nba@gmail.com>
2012-03-25 01:08:23 +0900

    use child process

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,14 @@
+var exec = require("child_process").exec;
+
 function start() {
   console.log("Request handler 'start' was called.");
+  var content = "empty";
 
-  function sleep(milliSecounds) {
-    var startTime = new Date().getTime();
-    while (new Date().getTime() < startTime + milliSecounds);
-  }
-
-  sleep(10000);
-  return "Hello Start";
+  exec("ls -lah", function(error, stdout, stderr){
+    content = stdout;
+  });
+  
+  return content;
 }
 
 function upload() {
